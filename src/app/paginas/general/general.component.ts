36e708d384022bf106514ccc468c0d4ec1469d7e
Bryan Sarmiento <bryansb.297@gmail.com>
2022-01-11 22:45:06 -0500

    fix: General and LifeCycle pages

--- src/app/paginas/general/general.component.ts
+++ src/app/paginas/general/general.component.ts
@@ -35,21 +35,21 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   `],
   animations: [
     trigger('animation', [
-        state('visible', style({
-            transform: 'translateX(0)',
-            opacity: 1
-        })),
-        transition('void => *', [
-            style({transform: 'translateX(50%)', opacity: 0}),
-            animate('300ms ease-out')
-        ]),
-        transition('* => void', [
-            animate(('250ms ease-in'), style({
-                height: 0,
-                opacity: 0,
-                transform: 'translateX(50%)'
-            }))
-        ])
+      state('visible', style({
+        transform: 'translateX(0)',
+        opacity: 1
+      })),
+      transition('void => *', [
+        style({ transform: 'translateX(50%)', opacity: 0 }),
+        animate('300ms ease-out')
+      ]),
+      transition('* => void', [
+        animate(('250ms ease-in'), style({
+          height: 0,
+          opacity: 0,
+          transform: 'translateX(50%)'
+        }))
+      ])
     ])
   ],
   templateUrl: "./general.component.html",
@@ -69,26 +69,25 @@ export class GeneralComponent implements OnInit, OnDestroy {
 
   languageOptions: any = [];
   languageSelected: string;
+  // -----
 
   // Referentes a los valores
-  // Crear modelo (?)
   identifierCatalog: any;
   identifierEntry: any;
   title: any[];
   description: any[];
   coverage: any[];
-  // ------
-
-  columns: any[];
+  keywords: any[];
+  // -----
 
   // Auxiliares
-  palabra: string;
-  palabraDialog: boolean;
+  word: string;
+  keywordDialog: boolean;
 
   constructor(
     private componentePrincipal: AppComponent,
     private lompadservice: LompadService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.loadGeneralObject();
@@ -109,31 +108,31 @@ export class GeneralComponent implements OnInit, OnDestroy {
     ];
 
     this.languageOptions = [
-      {label: "none", value: "none", code: "none"},
-      {label: "fra", value: "fra", code: "fra"},
-      {label: "fra-CA", value: "fra-CA", code: "fra-CA"},
-      {label: "fra-FR", value: "fra-FR", code: "fra-FR"},
-      {label: "eng", value: "eng", code: "eng"},
-      {label: "eng-AU", value: "eng-AU", code: "eng-AU"},
-      {label: "eng-CA", value: "eng-CA", code: "eng-CA"},
-      {label: "eng-GB", value: "eng-GB", code: "eng-GB"},
-      {label: "eng-US", value: "eng-US", code: "eng-US"},
-      {label: "deu", value: "deu", code: "deu"},
-      {label: "esl", value: "esl", code: "esl"},
-      {label: "ita", value: "ita", code: "ita"},
-      {label: "por", value: "por", code: "por"},
-      {label: "fr", value: "fr", code: "fr"},
-      {label: "fr-CA", value: "fr-CA", code: "fr-CA"},
-      {label: "fr-FR", value: "fr-FR", code: "fr-FR"},
-      {label: "en", value: "en", code: "en"},
-      {label: "en-AU", value: "en-AU", code: "en-AU"},
-      {label: "en-CA", value: "en-CA", code: "en-CA"},
-      {label: "en-GB", value: "en-GB", code: "en-GB"},
-      {label: "en-US", value: "en-US", code: "en-US"},
-      {label: "de", value: "de", code: "de"},
-      {label: "es", value: "es", code: "es"},
-      {label: "it", value: "it", code: "it"},
-      {label: "pt", value: "pt", code: "pt"},
+      { label: "none", value: "none", code: "none" },
+      { label: "fra", value: "fra", code: "fra" },
+      { label: "fra-CA", value: "fra-CA", code: "fra-CA" },
+      { label: "fra-FR", value: "fra-FR", code: "fra-FR" },
+      { label: "eng", value: "eng", code: "eng" },
+      { label: "eng-AU", value: "eng-AU", code: "eng-AU" },
+      { label: "eng-CA", value: "eng-CA", code: "eng-CA" },
+      { label: "eng-GB", value: "eng-GB", code: "eng-GB" },
+      { label: "eng-US", value: "eng-US", code: "eng-US" },
+      { label: "deu", value: "deu", code: "deu" },
+      { label: "esl", value: "esl", code: "esl" },
+      { label: "ita", value: "ita", code: "ita" },
+      { label: "por", value: "por", code: "por" },
+      { label: "fr", value: "fr", code: "fr" },
+      { label: "fr-CA", value: "fr-CA", code: "fr-CA" },
+      { label: "fr-FR", value: "fr-FR", code: "fr-FR" },
+      { label: "en", value: "en", code: "en" },
+      { label: "en-AU", value: "en-AU", code: "en-AU" },
+      { label: "en-CA", value: "en-CA", code: "en-CA" },
+      { label: "en-GB", value: "en-GB", code: "en-GB" },
+      { label: "en-US", value: "en-US", code: "en-US" },
+      { label: "de", value: "de", code: "de" },
+      { label: "es", value: "es", code: "es" },
+      { label: "it", value: "it", code: "it" },
+      { label: "pt", value: "pt", code: "pt" },
     ];
 
     this.objectOptions = this.componentePrincipal.objOptions;
@@ -144,19 +143,16 @@ export class GeneralComponent implements OnInit, OnDestroy {
 
   loadGeneralObject() {
     this.generalObject = this.lompadservice.objPricipal["DATA"]["general"];
-    // if (this.generalObject != undefined) {
-      
-    // }
   }
 
   setGeneralData() {
-    this.identifierCatalog = this.generalObject["Identifier"]["Catalog"];
-    this.identifierEntry = this.generalObject["Identifier"]["Entry"];
+    this.identifierCatalog = this.generalObject["Identifier"]["Catalog"][0];
+    this.identifierEntry = this.generalObject["Identifier"]["Entry"][0];
     this.title = this.generalObject["Title"]["Title"][0];
     this.description = this.generalObject["Description"]["Description"][0];
-    this.coverage = this.generalObject["Coverage"][0];
+    this.coverage = this.generalObject["Coverage"]["Coverage"][0];
 
-    this.structureSelected = this.generalObject["Structure"][0];
+    this.structureSelected = this.generalObject["Structure"]["Value"][0];
     this.aggregationLevelSelected = this.generalObject["Aggregation Level"]["Value"][0];
     this.languageSelected = this.generalObject["Language"]["Language"][0];
 
@@ -168,45 +164,45 @@ export class GeneralComponent implements OnInit, OnDestroy {
   }
 
   loadKeywords() {
-    this.columns = [];
-    let keys: [] = this.generalObject["keyword"];
+    this.keywords = [];
+    let keys: [] = this.generalObject["Keyword"]["Keyword"][0];
 
     if (keys != null) {
       keys.forEach((element) => {
         console.log("elementos ", element);
-        this.columns.push(element);
+        this.keywords.push(element);
       });
     }
   }
 
   addKeyword() {
-    this.palabraDialog = true;
+    this.keywordDialog = true;
   }
 
   removeLastKeyword() {
-    this.columns.splice(-1, 1);
+    this.keywords.splice(-1, 1);
   }
 
   cancel() {
-    this.palabraDialog = false;
+    this.keywordDialog = false;
   }
 
   saveNewKeyword() {
-    console.log(this.palabra);
-    this.palabraDialog = false;
-    this.columns.push(this.palabra);
-    this.palabra = "";
+    console.log(this.word);
+    this.keywordDialog = false;
+    this.keywords.push(this.word);
+    this.word = "";
   }
 
   changeStructure() {
-    this.generalObject["Structure"][0] = this.structureSelected;
+    this.generalObject["Structure"]["Value"][0] = this.structureSelected;
   }
 
   changeAggregationLevel() {
     this.generalObject["Aggregation Level"]["Value"][0] = this.aggregationLevelSelected;
   }
 
-  changeLanguage(){
+  changeLanguage() {
     this.generalObject["Language"]["Language"][0] = this.languageSelected;
   }
 
@@ -215,13 +211,15 @@ export class GeneralComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.generalObject["Identifier"]["Catalog"] = this.identifierCatalog;
-    this.generalObject["Identifier"]["Entry"] = this.identifierEntry;
+    this.generalObject["Identifier"]["Catalog"][0] = this.identifierCatalog;
+    this.generalObject["Identifier"]["Entry"][0] = this.identifierEntry;
     this.generalObject["Title"]["Title"][0] = this.title;
     this.generalObject["Description"]["Description"][0] = this.description;
-    this.generalObject["Coverage"][0] = this.coverage;
-    this.generalObject["keyword"] = this.columns;
+    this.generalObject["Coverage"]["Coverage"][0] = this.coverage;
+    this.generalObject["Keyword"]["Keyword"][0] = this.keywords;
+
     this.lompadservice.objPricipal["DATA"]["general"] = this.generalObject;
+
     console.log("Destroy General");
     this.lompadservice.saveObjectLompad(this.generalObject, "general");
   }
